2e59452718639c2c319ffdc18a3e3c9a624255fe
Veeran Puthumkara <vkuttyp@gmail.com>
2025-12-24 22:09:35 +0300

    some

--- layer/nuxt.config.ts
+++ layer/nuxt.config.ts
@@ -29,6 +29,9 @@ export default defineNuxtConfig({
     enabled: true,
   },
   content: {
+    experimental: {
+      sqliteConnector: 'native',
+    },
     build: {
       markdown: {
         highlight: {
